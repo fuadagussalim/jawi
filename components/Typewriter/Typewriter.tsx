44ad6f5ab989589b1e72331ffdba4b99cf644ab4
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-03-27 23:14:14 +0700

    resolve bug and aupdate blog page

--- components/Typewriter/Typewriter.tsx
+++ components/Typewriter/Typewriter.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import {Typewriter} from "react-simple-typewriter";
+import React, { useState, useEffect } from 'react';
 
 interface TypewriterContentProps {
   content: string;
 }
 
-const TypewriterContent: React.FC<TypewriterContentProps> = ({
-  content,
-}) => {
-  return (
-    <span>
-      <Typewriter
-        words={[content]}
-        loop={Infinity}
-        cursor
-        cursorStyle="|"
-        typeSpeed={75}
-      />
-    </span>
-  );
+const TypewriterContent: React.FC<TypewriterContentProps> = ({ content }: TypewriterContentProps) => {
+  const [displayedContent, setDisplayedContent] = useState('');
+
+  useEffect(() => {
+    const typeContent = async () => {
+      // Simulate typing effect
+      for (let i = 0; i <= content.length; i++) {
+        setDisplayedContent(content.substring(0, i));
+        await new Promise(resolve => setTimeout(resolve, 100)); // Adjust typing speed as needed
+      }
+    };
+
+    typeContent();
+  }, [content]);
+
+  return <>{displayedContent}</>;
 };
 
 export default TypewriterContent;
