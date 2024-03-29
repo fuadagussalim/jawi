e776bea3930a1c049c96ad6b1e22551047c5a00c
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-03-29 19:55:23 +0700

    implement mobile

--- pages/ClientPage.tsx
+++ pages/ClientPage.tsx
@@ -336,20 +336,26 @@ export default function ClientPage({ allPosts}) {
                                                     </CardBody>
                                                 </Card>
                                                 <CardImage src='/manajemen-data-jawi.png' className='gridcol-span-1 h-64 sm:block md:hidden' imageClassName={' object-cover objet-bottom'} alt={''} />
-                                                <Card className="h-full rounded-none  grid col-span-1 align-middle text-primary-400">
-                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] p-20">
-                                                        <div className="w-full">
+                                                <Card className="grid h-full rounded-none  sm:mb-10 md:mb-0 col-span-1 align-middle text-primary-400">
+                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] m-10">
+                                                        <div className="w-full sm:pt-5  rounded-0">
                                                             <div>
 
-                                                                <h3 className="text-black w-full text-center m-0 text-base font-extrabold">MANAJEMEN DATA</h3>
+                                                                <h3 className="text-black w-full text-center m-0 text-base font-extrabold">SURVEI BIODIVERSITAS</h3>
                                                                 <p className="text-black text-center m-0 max-w-xs mt-2 text-base sm:max-w-full">Hutan Kemuning,</p>
                                                             </div>
 
-                                                            <div className="flex justify-center">
-                                                                <Button variant="orange" className="self-auto bg-[#F3F5F8] text-[#faa51a] rounded-5 items-center mt-5">
-                                                                    READ MORE
+                                                            <div className="flex justify-center pt-5">
+                                                                {/* <Link type="button" className="uppercase text-[#faa51a] uppercase font-bold px-4 py-2 hover:bg-[#faa51a] hover:text-white " href="/detail/site-projek" passHref={true}> */}
+
+                                                                    <Button variant="orange" >
+                                                                         Read More
                                                                     </Button>
-                                                                    </div>
+
+
+                                                                {/* </Link> */}
+                                                                {/* <Button className="self-auto bg-[#F3F5F8] text-[#faa51a] rounded-5 items-center mt-5">READ MORE</Button> */}
+                                                                </div>
 
 
                                                         </div>
@@ -358,41 +364,60 @@ export default function ClientPage({ allPosts}) {
                                                 </Card>
                                                 <CardImage src='/manajemen-data-jawi.png' className='gridcol-span-1 h-64 sm:hidden md:block' imageClassName={' object-cover objet-bottom'} alt={''} />
                                                 <CardImage src='/model-OU-17052023-1.jpg' className='gridcol-span-1 h-64' imageClassName={'object-cover objet-bottom'} alt={''} />
-                                                <Card className="h-full rounded-none  grid col-span-1 align-middle text-primary-400">
-                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] p-20">
-                                                        <div className="w-full">
+                                                <Card className="grid h-full rounded-none  sm:mb-10 md:mb-0 col-span-1 align-middle text-primary-400">
+                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] m-10">
+                                                        <div className="w-full sm:pt-5  rounded-0">
                                                             <div>
 
-                                                                <h3 className="text-black w-1/2 text-center mx-auto my-0 text-base font-extrabold">PEMETAAN DAN PEMODELAN SPASIAL</h3>
+                                                                <h3 className="text-black w-full text-center m-0 text-base font-extrabold">SURVEI BIODIVERSITAS</h3>
                                                                 <p className="text-black text-center m-0 max-w-xs mt-2 text-base sm:max-w-full">Hutan Kemuning,</p>
                                                             </div>
 
-                                                            <div className="flex justify-center">
-                                                                <Button variant='orange' className=" mt-5">READ MORE</Button></div>
+                                                            <div className="flex justify-center pt-5">
+                                                                {/* <Link type="button" className="uppercase text-[#faa51a] uppercase font-bold px-4 py-2 hover:bg-[#faa51a] hover:text-white " href="/detail/site-projek" passHref={true}> */}
+
+                                                                    <Button variant="orange" >
+                                                                         Read More
+                                                                    </Button>
+
+
+                                                                {/* </Link> */}
+                                                                {/* <Button className="self-auto bg-[#F3F5F8] text-[#faa51a] rounded-5 items-center mt-5">READ MORE</Button> */}
+                                                                </div>
 
 
                                                         </div>
 
                                                     </CardBody>
                                                 </Card>
-                                                <Card className="h-full rounded-none  grid col-span-1 align-middle text-primary-400">
-                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] p-20">
-                                                        <div className="w-full">
+                                                <CardImage src='/penyusunan-dokumen-pengelolaan-JAWI.png' className='gridcol-span-1 h-64 md:hidden sm:block' imageClassName={'object-cover objet-bottom'} alt={''} />
+                                                <Card className="grid h-full rounded-none  sm:mb-10 md:mb-0 col-span-1 align-middle text-primary-400">
+                                                    <CardBody className="flex align-middle h-full items-center align-center bg-[#F3F5F8] m-10">
+                                                        <div className="w-full sm:pt-5  rounded-0">
                                                             <div>
 
-                                                                <h3 className="text-black h-full w-1/2 text-center mx-auto my-0 text-base font-extrabold">PEMETAAN DAN PEMODELAN SPASIAL</h3>
+                                                                <h3 className="text-black w-full text-center m-0 text-base font-extrabold">SURVEI BIODIVERSITAS</h3>
                                                                 <p className="text-black text-center m-0 max-w-xs mt-2 text-base sm:max-w-full">Hutan Kemuning,</p>
                                                             </div>
 
-                                                            <div className="flex justify-center">
-                                                                <Button variant='orange' className="self-auto bg-[#F3F5F8] text-[#faa51a] rounded-5 items-center mt-5">READ MORE</Button></div>
+                                                            <div className="flex justify-center pt-5">
+                                                                {/* <Link type="button" className="uppercase text-[#faa51a] uppercase font-bold px-4 py-2 hover:bg-[#faa51a] hover:text-white " href="/detail/site-projek" passHref={true}> */}
+
+                                                                    <Button variant="orange" >
+                                                                         Read More
+                                                                    </Button>
+
+
+                                                                {/* </Link> */}
+                                                                {/* <Button className="self-auto bg-[#F3F5F8] text-[#faa51a] rounded-5 items-center mt-5">READ MORE</Button> */}
+                                                                </div>
 
 
                                                         </div>
 
                                                     </CardBody>
                                                 </Card>
-                                                <CardImage src='/penyusunan-dokumen-pengelolaan-JAWI.png' className='gridcol-span-1 h-64' imageClassName={'object-cover objet-bottom'} alt={''} />
+                                                <CardImage src='/penyusunan-dokumen-pengelolaan-JAWI.png' className='gridcol-span-1 h-64 sm:hidden md:block' imageClassName={'object-cover objet-bottom'} alt={''} />
                                             </CardGroup>
                                         </div>
 
