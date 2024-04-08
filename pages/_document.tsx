e5d966d010a6db9ece178d0fdf970e30eda6ce59
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-04-08 23:54:32 +0700

    checkpoint 2.9: add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,14 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Html, Head, Main, NextScript } from "next/document";
 
 
-
+import { Analytics } from "@vercel/analytics/react"
 export default function Document() {
   return (
     <Html lang="en" >
       <Head><link rel="icon" href="/favicon.ico" sizes="any" /></Head>
       <body>
          
+        <Analytics />
 
         <Main/>
 
