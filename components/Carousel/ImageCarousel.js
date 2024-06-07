d10b42b7d27c88effd1878452aee16c53d88bded
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-06-08 03:47:53 +0700

    carousel

--- components/Carousel/ImageCarousel.js
+++ components/Carousel/ImageCarousel.js
@@ -7,32 +7,45 @@ import Link from "next/link";
 
 const ImageCarousel = ({carouselPosts, className=""}) => {
     const posts = carouselPosts;
-
+    console.log("post", posts)
     const [currentImageIndex, setCurrentImageIndex] = useState(1);
-    const [previousImageIndex, setpreviousImageIndex] = useState(currentImageIndex - 1 < 1 ? 4 : currentImageIndex - 1);
-    const [afterImageIndex, setafterImageIndex] = useState(currentImageIndex + 1 > 4 ? 0 : currentImageIndex + 1);
+    const [previousImageIndex, setpreviousImageIndex] = useState(currentImageIndex - 1 < 0 ? carouselPosts.length-2 : currentImageIndex - 1);
+    const [afterImageIndex, setafterImageIndex] = useState(currentImageIndex + 1 > carouselPosts.length ? 0 : currentImageIndex + 1);
    
     function handlePrevClick() {
         // Check if at the first slide
-        if (currentImageIndex === 1) {
-            setCurrentImageIndex(4);
+        if (previousImageIndex === 0) {
+            setpreviousImageIndex(carouselPosts.length-2);
+            setCurrentImageIndex(0);
+            setafterImageIndex(1);
+        } else {
+
+            setpreviousImageIndex((prev) => (prev - 1 < 0 ? carouselPosts.length-2 : prev - 1));
+            setafterImageIndex(currentImageIndex);
+            setCurrentImageIndex(previousImageIndex);
         }
 
-        setpreviousImageIndex((prev) => (prev - 1 < 1 ? 4 : prev - 1));
-        setafterImageIndex(currentImageIndex);
-        setCurrentImageIndex(previousImageIndex);
     }
 
     function handleNextClick() {
         // Check if at the last slide
-        if (currentImageIndex === 4) {
-            setCurrentImageIndex(1);
+        if (afterImageIndex === carouselPosts.length-1) {
+            setpreviousImageIndex(carouselPosts.length-2);
+            setCurrentImageIndex(0);
+            setafterImageIndex(1);
+        } else {
+            setpreviousImageIndex(currentImageIndex);
+            setafterImageIndex((next) => (next + 1 > carouselPosts.length? 1 : next + 1));
+            setCurrentImageIndex(afterImageIndex);
         }
-
-        setpreviousImageIndex(currentImageIndex);
-        setafterImageIndex((next) => (next + 1 > 4 ? 1 : next + 1));
-        setCurrentImageIndex(afterImageIndex);
+        
+        // setpreviousImageIndex(currentImageIndex);
+        // setafterImageIndex((next) => (next + 1 > carouselPosts.length ? 1 : next + 1));
+        // setCurrentImageIndex(afterImageIndex);
+        
+        // posts[previousImageIndex]?.
     }
+    console.log("posts[previousImageIndex]?",previousImageIndex);
 
     return (
         <div id="magazine" className={`sm:mb-10 md:mb-10 ${className}`}>
@@ -47,32 +60,32 @@ const ImageCarousel = ({carouselPosts, className=""}) => {
                     ←
                 </button>
                 <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-8">
-                        <Link href={`/posts/${posts[previousImageIndex].slug}`}>
+                        <Link href={`/posts/${posts[previousImageIndex]?.slug}`}>
                     <div className="hover:shadow-xl relative hidden md:show grid col-span-1 w-[400] h-96 grid col-span-1 overflow-hidden">
                         <h5 className="absolute font-bold text-shadow text-white z-20 bottom-[30px] m-5">
-                            {posts[previousImageIndex].title}
+                            {posts[previousImageIndex]?.title}
                         </h5>
-                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[previousImageIndex].date}</p>
+                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[previousImageIndex]?.date}</p>
                         
-                        <img src={posts[previousImageIndex].image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
+                        <img src={posts[previousImageIndex]?.image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
                     </div>
                         </Link>
-                        <Link href={`/posts/${posts[currentImageIndex].slug}`}>
+                        <Link href={`/posts/${posts[currentImageIndex]?.slug}`}>
                     <div className="hover:shadow-xl relative grid col-span-1 w-[400] h-96 grid col-span-1 overflow-hidden">
-                        <h5 className="absolute font-bold drop-shadow-lg text-white z-20 bottom-[30px] m-5">{posts[currentImageIndex].title}</h5>
-                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[currentImageIndex].date}</p>
+                        <h5 className="absolute font-bold drop-shadow-lg text-white z-20 bottom-[30px] m-5">{posts[currentImageIndex]?.title}</h5>
+                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[currentImageIndex]?.date}</p>
                         
-                        <img src={posts[currentImageIndex].image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
+                        <img src={posts[currentImageIndex]?.image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
                     </div>
                         </Link>
-                        <Link href={`/posts/${posts[afterImageIndex].slug}`}>
+                        <Link href={`/posts/${posts[afterImageIndex]?.slug}`}>
                     <div className="hover:shadow-xl relative hidden md:show grid col-span-1 w-[400] h-96 grid col-span-1 overflow-hidden">
                         <h5 className="absolute font-bold text-white z-20 bottom-[30px] m-5">
-                            {posts[afterImageIndex].title}
+                            {posts[afterImageIndex]?.title}
                         </h5>
-                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[afterImageIndex].date}</p>
+                        <p className="absolute text-white font-thin z-20 bottom-[10px] m-5">{posts[afterImageIndex]?.date}</p>
                         
-                        <img src={posts[afterImageIndex].image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
+                        <img src={posts[afterImageIndex]?.image} alt="" className="absolute w-full h-full object-cover transition duration-500" />
                     </div>
                         </Link>
                 </div>
