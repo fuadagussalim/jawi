ab0c3abf0f1b89567bc7ca94ce1a852a0f63c861
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-06-08 01:49:53 +0700

    staticPageGenerationTimeout: 1000

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const { protocol, hostname, port, pathname } = new URL(
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
- 
+ staticPageGenerationTimeout: 1000,
   images: {
     domains: ['blog.jawi.or.id', 'secure.gravatar.com', 'picsum.photos'],
     remotePatterns: [
